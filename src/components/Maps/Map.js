ba4d349f6d7180b2ca9df26c770078f9e4f2928c
LEECHANHYUNG <lsj721538@gmail.com>
2022-10-24 23:23:56 +0900

    add current location btn

--- src/components/Maps/Map.js
+++ src/components/Maps/Map.js
@@ -1,4 +1,8 @@
-import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
+import {
+  faLocationCrosshairs,
+  faMinus,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { Fragment, useEffect, useState } from "react";
 
@@ -13,14 +17,23 @@ const Map = () => {
   const zoomOut = () => {
     map.setLevel(map.getLevel() + 1);
   };
-
+  const getCurrentLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const lat = position.coords.latitude,
+          lng = position.coords.longitude;
+        const currentLocation = new kakao.maps.LatLng(lat, lng);
+        map.panTo(currentLocation);
+      });
+    }
+  };
   useEffect(() => {
     kakao.maps.load(() => {
+      const container = document.getElementById("map");
       const mainLocation = new kakao.maps.LatLng(
         37.523383097083546,
         127.05459973415205
       );
-      const container = document.getElementById("map");
       const options = {
         center: mainLocation,
         level: 3,
@@ -55,6 +68,12 @@ const Map = () => {
           </span>
           <span onClick={zoomOut}>
             <FontAwesomeIcon icon={faMinus} className={classes.icon} />
+          </span>
+          <span onClick={getCurrentLocation}>
+            <FontAwesomeIcon
+              icon={faLocationCrosshairs}
+              className={classes.icon}
+            />
           </span>
         </div>
       </div>
