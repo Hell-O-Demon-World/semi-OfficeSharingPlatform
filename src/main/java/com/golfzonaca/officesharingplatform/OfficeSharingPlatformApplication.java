98b93724c493b4eedfbac10e127fae5581ca3ead
ryeongee <ryeongee24@gmail.com>
2022-10-29 16:06:56 +0900

    Update CORS issue
    Update CORS issue code

--- src/main/java/com/golfzonaca/officesharingplatform/OfficeSharingPlatformApplication.java
+++ src/main/java/com/golfzonaca/officesharingplatform/OfficeSharingPlatformApplication.java
@@ -2,12 +2,15 @@ package com.golfzonaca.officesharingplatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class OfficeSharingPlatformApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OfficeSharingPlatformApplication.class, args);
-	}
 
+	}
 }
