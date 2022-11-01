26ed0baa706d13830a9665b66fdfb685f73ee5df
ryeongee <ryeongee24@gmail.com>
2022-11-01 10:51:06 +0900

    Update PlaceController.java

--- src/main/java/com/golfzonaca/officesharingplatform/web/reservation/PlaceController.java
+++ src/main/java/com/golfzonaca/officesharingplatform/web/reservation/PlaceController.java
@@ -3,6 +3,7 @@ package com.golfzonaca.officesharingplatform.web.reservation;
 import com.golfzonaca.officesharingplatform.domain.Place;
 import com.golfzonaca.officesharingplatform.domain.Room;
 import com.golfzonaca.officesharingplatform.repository.PlaceRepository;
+import com.golfzonaca.officesharingplatform.repository.room.RoomKindRepository;
 import com.golfzonaca.officesharingplatform.repository.room.RoomRepository;
 import com.golfzonaca.officesharingplatform.web.reservation.form.ReservationViewFrom;
 import lombok.RequiredArgsConstructor;
@@ -20,23 +21,23 @@ import java.util.Map;
 public class PlaceController {
     private final PlaceRepository placeRepository;
     private final RoomRepository roomRepository;
-    private final RoomKindRepository roomKindRepository;
+//    private final RoomKindRepository roomKindRepository;
 
-    @GetMapping("/{placeId}")
-    public ReservationViewFrom reservationView(@PathVariable long placeId) {
-        ReservationViewFrom reservationViewFrom = new ReservationViewFrom();
-        Map<Long, Room> roomMap = roomRepository.findRoomsByPlaceId(placeId);
-        Iterator<Long> keys = roomMap.keySet().iterator();
-        while( keys.hasNext() ){
-            Long key = keys.next();
-            Room storedRoom = roomMap.get(key);
-            if (storedRoom.getRoomKindId() == (placeId)){
-                store.put(key, storedRoom);
-            }
-        }
-        reservationViewFrom.setDesk();
-        return reservationViewFrom;
-    }
+//    @GetMapping("/{placeId}")
+//    public ReservationViewFrom reservationView(@PathVariable long placeId) {
+//        ReservationViewFrom reservationViewFrom = new ReservationViewFrom();
+//        Map<Long, Room> roomMap = roomRepository.findRoomsByPlaceId(placeId);
+//        Iterator<Long> keys = roomMap.keySet().iterator();
+//        while( keys.hasNext() ){
+//            Long key = keys.next();
+//            Room storedRoom = roomMap.get(key);
+//            if (storedRoom.getRoomKindId() == (placeId)){
+//                store.put(key, storedRoom);
+//            }
+//        }
+//        reservationViewFrom.setDesk();
+//        return reservationViewFrom;
+//    }
     @PostMapping("/{placeId}")
     public String reservationForm(@PathVariable long placeId, Model model) {
         Place place = placeRepository.findById(placeId);
