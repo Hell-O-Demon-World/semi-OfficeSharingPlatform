dd773b171e4aed23719906be369f1b9a9e93d9f9
ryeongee <ryeongee24@gmail.com>
2022-11-02 10:37:36 +0900

    Update HomeController

---
+++ src/main/java/com/golfzonaca/adminpage/web/MainController.java
@@ -0,0 +1,26 @@
+package com.golfzonaca.adminpage.web;
+
+import com.golfzonaca.adminpage.domain.Company;
+import com.golfzonaca.adminpage.repository.CompanySearchCond;
+import com.golfzonaca.adminpage.service.CompanyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class MainController {
+    private final CompanyService companyService;
+
+    @GetMapping("/")
+    public String mainPage(@ModelAttribute("companySearch") CompanySearchCond companySearch, Model model) {
+        List<Company> companies = companyService.findCompanies(companySearch);
+        model.addAttribute("companies", companies);
+        return "company/companies";
+    }
+}
